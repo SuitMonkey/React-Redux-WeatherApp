9466ef250f0063318c444e430f488873cf9ac2c9
Francis Caceres (ClaroDom) <Francis Caceres@corp.codetel.com.do>
2018-03-15 11:36:01 -0400

    Initial Commit.

---
+++ src/containers/weather_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+
+    renderWeather(cityData){
+        <tr>
+            <td>
+                {cityData.city.name}
+            </td>
+        </tr>
+    }
+
+    render() {
+        return (
+            <table className=" table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    //Using weather because we explicitly use it like that
+    // in index.js from reducers
+    return { weather: state.weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,14 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return state.concat([action.payload.data]);
+            //You can also do it like this:
+            // return [action.payload.data, ...state];
+            //how it does work: [city, city, city]
+            // how it does not work: [city, [city,city]]
+    }
+
+    return state;
+}
